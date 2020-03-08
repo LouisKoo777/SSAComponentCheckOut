c6ef2644f9933c1ddba09cbd82c8851aa57b653f
GuJunBing <>
2020-03-08 18:05:32 +0800

    初始化项目

---
+++ Example/Pods/Target Support Files/Pods-SSAComponentCheckOut_Example/Pods-SSAComponentCheckOut_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SSAComponentCheckOut_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SSAComponentCheckOut_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-SSAComponentCheckOut_Example/Pods-SSAComponentCheckOut_Example-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SSAComponentCheckOut_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SSAComponentCheckOut_ExampleVersionString[];
+
